4ce3308c22b52b9abdf05d795174541f1948e327
edulol-1 <edu3000@ciencias.unam.mx>
2025-11-19 14:04:42 -0600

    Add class for handling the TUI

---
+++ src/window_drawer.py
@@ -0,0 +1,89 @@
+import curses
+import sys
+from curses.textpad import Textbox, rectangle
+
+class WindowDrawer:
+    def __init__(self):
+        self.screen = curses.initscr()
+        curses.noecho()
+        curses.curs_set(1)
+        
+        # Set the size of all the upper part of the interface
+        self.upper_rect_h = int(curses.LINES * 0.8)
+        self.upper_rect_w = int(curses.COLS * 0.99)
+        self.lower_rect_h = int(curses.LINES * 0.2)
+        self.lower_rect_w = self.upper_rect_w
+
+        self.out_messages_win_h = int(curses.LINES * 0.7)
+        self.out_messages_win_w = int(curses.COLS * 0.9)
+        self.out_messages_win_uly = int(curses.LINES * 0.07)
+        self.out_messages_win_ulx = int(curses.COLS * 0.05)
+
+        self.messages_win_h = int(curses.LINES * 0.65)
+        self.messages_win_w = int(curses.COLS * 0.8)
+        self.messages_win_uly = int(curses.LINES * 0.1)
+        self.messages_win_ulx = int(curses.COLS * 0.1)
+
+        # Set the size of the rectangle and the windows in the lower part
+        self.out_chat_win_h = int(curses.LINES * 0.17)
+        self.out_chat_win_w = int(curses.COLS * 0.9)
+        self.out_chat_win_uly = int(curses.LINES * (0.8 + 0.05))
+        self.out_chat_win_ulx = int(curses.COLS * 0.05)
+
+        self.chat_win_h = int(curses.LINES * 0.09)
+        self.chat_win_w = int(curses.COLS * 0.8)
+        self.chat_win_uly = int(curses.LINES * (0.8 + 0.08))
+        self.chat_win_ulx = int(curses.COLS * 0.1)
+
+        self.out_messages_win = None
+        self.out_chat_win = None
+        self.messages_win = None
+        self.chat_win = None
+        self.textbox = None
+
+    def set_rectangles(self):
+        rectangle(self.screen, 0, 0, self.upper_rect_h, self.upper_rect_w)
+        rectangle(self.screen, self.upper_rect_h, 0, self.upper_rect_h + self.lower_rect_h, self.lower_rect_w)
+        self.screen.refresh()
+
+    def set_windows(self):
+        self.messages_win = curses.newwin(self.messages_win_h, self.messages_win_w,
+                                          self.messages_win_uly, self.messages_win_ulx)
+        self.out_messages_win = curses.newwin(self.out_messages_win_h, self.out_messages_win_w,
+                                                  self.out_messages_win_uly, self.out_messages_win_ulx)
+        self.chat_win = curses.newwin(self.chat_win_h, self.chat_win_w,
+                                      self.chat_win_uly, self.chat_win_ulx)
+        self.out_chat_win = curses.newwin(self.out_chat_win_h, self.out_chat_win_w,
+                                          self.out_chat_win_uly, self.out_chat_win_ulx)
+        self.out_messages_win.border("|", "|", "-", "-", "+", "+", "+", "+")
+        self.out_chat_win.border("|", "|", "-", "-", "+", "+", "+", "+")
+        self.messages_win.refresh()
+        self.out_messages_win.refresh()
+        self.chat_win.refresh()
+        self.out_chat_win.refresh()
+
+    def set_chatbox(self):
+        self.textbox = Textbox(self.chat_win)
+        self.textbox.edit()
+        outgoing_msg = self.textbox.gather()
+        self.messages_win.addstr(0, 0, outgoing_msg)
+        self.messages_win.refresh()
+
+    def print_message(self):
+        pass
+
+    def start_interface(self):
+        try:
+            self.set_rectangles()
+            self.set_windows()
+            self.set_chatbox()
+            curses.napms(3000)
+            curses.echo()
+            curses.endwin()
+        except Exception as err:
+            print(type(err))
+            print(f"XD {err}")
+            print("Terminal should be at least 100x33 in size!")
+        
+drawer = WindowDrawer()
+drawer.start_interface()
