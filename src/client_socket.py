f273df8f9d7678845e0d78fbaa4047ede55ac7fa
edulol-1 <edu3000@ciencias.unam.mx>
2025-11-25 12:34:54 -0600

    Remove spurious comments

--- src/client_socket.py
+++ src/client_socket.py
@@ -21,35 +21,10 @@ class ClientSocket:
         received_message = data.decode("utf-8")
         return received_message
 
-    #def receive_and_show_continuous_msgs(self):
-        #self.receive_and_show_msg()
-        #        while True:
-        #            try:
-        #                self.receive_and_show_msg()
-        #            except IOError as e:
-        #                self.interface.print_message(str(e))
-        #                self.terminate = True
-        #                break
-        #            except KeyboardInterrupt:
-        #                print("See you later :)")
-        #                self.terminate = True
-        #                break
-        #            except Exception as err:
-        #                print(f"{err}")
-        #                self.terminate = True
-        #                break
-
     def send_message(self, message):
         if message != "" and message is not None:
             message = message.encode("utf-8")
             self.client_sock.send(message)
-
-#    def send_continuous_messages(self):
-#        while not self.terminate:
-#            message = self.interface.type_and_get_message()
-#            self.interface.print_message(message)
-#            message = message.encode("utf-8")
-#            self.client_sock.send(message)
 
     def close(self):
         self.client_sock.close()
