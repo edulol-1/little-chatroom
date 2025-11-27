cf97cfb70ae49e5e0b3d55b4c70c7f4c0f41877f
edulol-1 <edu3000@ciencias.unam.mx>
2025-11-27 14:22:35 -0600

    Add logic for handling user input, receiving and sending messages in two different threads.

--- src/chatroom.py
+++ src/chatroom.py
@@ -1,6 +1,7 @@
 import sys
 import errno
 import threading
+import curses
 from interface_handler import InterfaceHandler
 from client_socket import ClientSocket
 from queue import Queue
@@ -9,7 +10,7 @@ class Chatroom:
     def __init__(self):
         self.client_sock = ClientSocket()
         self.interface = InterfaceHandler()
-        self.commands = Queue()
+        self.messages_q = Queue()
         self.exit_f = Queue()
 
     def start(self):
@@ -21,13 +22,47 @@ class Chatroom:
         self.interface.close()
 
     def receive_messages(self):
-        while not exit_f.empty():
+        while self.exit_f.empty():
             message = self.client_sock.receive_message()
-            self.commands.put((print_message, message))
+            self.messages_q.put(message)
 
     def send_and_print_messages(self):
+        input_buff = ""
         while True:
-            while not self.incoming_messages.empty():
-                self.interface.print_message(self.incoming_messages.get())
-            message = self.interface.type_and_get_message()
-            #asyncio?
+            while not self.messages_q.empty():
+                try:
+                    self.interface.print_message(self.messages_q.get_nowait())
+                except Queue.Empty:
+                    pass
+
+            ch = self.interface.type_and_get_ch()
+            if ch == -1:
+                continue
+            if ch in (10, 13):
+                if input_buff.lstrip() == "exit":
+                    self.exit_f.put(1)
+                    break
+                self.interface.print_message(input_buff)
+                self.client_sock.send_message(input_buff)
+                input_buff = ""
+                self.interface.clear_chat_win()
+            elif ch in (curses.KEY_BACKSPACE, 8, 127):
+                input_buff = input_buff[:-1]
+                self.interface.clear_chat_win()
+                self.interface.buffer_to_chat(input_buff)
+            else:
+                input_buff += chr(ch)
+                self.interface.buffer_to_chat(input_buff)
+
+if __name__ == "__main__":
+    chatroom = Chatroom()
+    try:
+        chatroom.start()
+        t_recv = threading.Thread(target=chatroom.receive_messages, daemon=True)
+        t_recv.start()
+        chatroom.send_and_print_messages()
+        t_recv.join()
+    except Exception as e:
+        print(f"{e}")
+    finally:
+        chatroom.close()
