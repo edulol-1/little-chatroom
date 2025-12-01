30b82b86748c193fa7ef0a5fec678893516c9e9b
edulol-1 <edu3000@ciencias.unam.mx>
2025-11-30 18:40:30 -0600

    Several changes in the class ClientSocket: - Change this socket to be non-blocking, both when sending and receiving - At receive_message() add try-except block for a non-blocking behavior - At send_message() add also a try-except block for a non-blocking   behavior

--- src/client_socket.py
+++ src/client_socket.py
@@ -2,6 +2,7 @@ import socket
 import sys
 import errno
 import threading
+import select
 
 class ClientSocket:
     def __init__(self, server_ip="127.0.0.1", server_port=10000):
@@ -11,19 +12,34 @@ class ClientSocket:
 
     def connect(self):
         self.client_sock.connect((self.server_ip, self.server_port))
+        self.client_sock.setblocking(0)
 
     def receive_message(self):
-        data = self.client_sock.recv(1024)
-        if len(data) == 0:
+        message = ""
+        while True:
+            try:
+                message = self.client_sock.recv(1024)
+                break
+            except socket.error as e:
+                err = e.args[0]
+                if err == errno.EAGAIN or err == errno.EWOULDBLOCK:
+                    continue
+        if len(message) == 0:
             raise IOError("Connection closed by server!")
-
-        received_message = data.decode("utf-8")
-        return received_message
+        return message.decode("utf-8")
 
     def send_message(self, message):
-        if message != "" and message is not None:
-            message = message.encode("utf-8")
-            self.client_sock.send(message)
+        total_sent = 0
+        message = message.encode("utf-8")
+        while len(message):
+            try:
+                sent = self.client_sock.send(message)
+                total_sent += sent
+                message = message[sent:]
+            except socket.error as e:
+                if e.errno != errno.EAGAIN:
+                    raise e
+                select.select([], [self.client_sock], [])
 
     def close(self):
         self.client_sock.close()
