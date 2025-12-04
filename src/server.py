315d9d4c483e857d592862d649c14d499a625b32
edulol-1 <edu3000@ciencias.unam.mx>
2025-12-04 01:12:23 -0600

    Add logic for username: - New dictionary to store the usernames - Asks for username when receiving a new connection - Forwards the messages including the username of the sender

--- src/server.py
+++ src/server.py
@@ -20,8 +20,16 @@ server_sock.bind((ip_address, port))
 # Listen for incomming connections
 server_sock.listen(5)
 
-# List of sockets to be monitored by selec()
+# List of sockets to be monitored by select()
 socket_list = [server_sock]
+
+# Usernames and its corresponding socket object
+socket_dict = {server_sock.fileno(): ""}
+
+def get_username(sock):
+    username = sock.recv(1024)
+    username = username.decode()
+    return username
 
 while True:
     readable, _, exceptional = select.select(socket_list, [], socket_list)
@@ -31,21 +39,27 @@ while True:
         if sock is server_sock:
             conn, address = server_sock.accept()
             socket_list.append(conn)
+            socket_dict[conn.fileno()] = get_username(conn)
+            # add the user (header) of the new user
             print(f"New connection received from {address}")
         else:
             data = sock.recv(1024)
             if not data:
                 socket_list.remove(sock)
+                del socket_dict[sock.fileno()]
                 sock.close()
                 continue
 
+            message = socket_dict[sock.fileno()] + " > " + data.decode()
+            message = message.encode()
             for s in socket_list:
                 if (s is not server_sock) and (s is not sock):
-                    s.send(data)
+                    s.send(message)
 
     for sock in exceptional:
         if sock in socket_list:
             socket_list.remove(sock)
+            del socket_dict[sock.fileno()]
             sock.close()
             print(f"Socket {sock} disconnected!")
 
